a0249451b2f89ee9e2348ec98986bed81b3ac1e6
olgafrunza <104467572+olgafrunza@users.noreply.github.com>
2023-02-04 23:05:34 -0500

    Add files via upload

---
+++ Random Choice/script.js
@@ -0,0 +1,60 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+  if (e.key === "Enter") {
+    randomSelect();
+    e.target.value = "";
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.innerText = tag;
+    tagEl.classList.add("tag");
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 10;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tagsEl = document.querySelectorAll(".tag");
+  return tagsEl[Math.floor(Math.random() * tagsEl.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
